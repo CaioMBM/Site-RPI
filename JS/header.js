380b0bdcefd646e150213974bf8612fed66d9ee0
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-14 20:19:24 -0300

    Adição da responsividade no header (769px-980px)
    - Tanto o CSS quanto o JS foi feito para essa medida de tela.

---
+++ JS/header.js
@@ -0,0 +1,17 @@
+const navbar          = document.querySelector('nav')
+const icon_abrirMenu  = document.querySelector('.icon-abrirMenu');
+const icon_fecharMenu = document.querySelector('.icon-fecharMenu');
+
+// Abrir menu hamburguer
+icon_abrirMenu.addEventListener("click", ()=> {
+    icon_abrirMenu.style.display  = 'none';
+    icon_fecharMenu.style.display = 'block';
+    navbar.style.display          = 'block';
+})
+
+// Fechar menu hamburguer
+icon_fecharMenu.addEventListener("click", ()=> {
+    icon_abrirMenu.style.display  = 'block';
+    icon_fecharMenu.style.display = 'none';
+    navbar.style.display          = 'none';
+})
