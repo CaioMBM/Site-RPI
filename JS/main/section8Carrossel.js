fe0d4146077c669451b876de677b9d656fa05479
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-15 18:16:00 -0300

    Adição da responsividade na section 8 (599px-768px)

---
+++ JS/main/section8Carrossel.js
@@ -0,0 +1,45 @@
+const btnSec8CarrosselEsquerda = document.querySelectorAll('.botaoCarrosselDepoimentos')[0];
+const btnSec8CarrosselDireita = document.querySelectorAll('.botaoCarrosselDepoimentos')[1];
+const containerSec8 = document.querySelector('#section8Cards')
+
+// Media query
+/*
+Tela: ...px-389px = telaMuitoPequena
+Tela: 390px-458px = telaPequena
+Tela: 459px-598px = telaNormalPequena
+Tela: 599px-768px = telaNormal
+*/
+const telaNormal = window.matchMedia('(min-width: 599px) and (max-width: 768px)');
+
+
+function moverDireitaDepoimentos(){
+    const scrollMax = containerSec8.scrollWidth - containerSec8.clientWidth;
+
+    // Telas: 599px-768px
+    if (telaNormal.matches){
+        // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
+        if (containerSec8.scrollLeft >= scrollMax) {
+            containerSec8.scrollLeft = 0;
+        } 
+        // Se não for o último card, será mostrado o card seguinte
+        else {
+            containerSec8.scrollLeft += 250; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card.
+        }
+    }
+}
+btnSec8CarrosselDireita.addEventListener('click', moverDireitaDepoimentos);
+
+function moverEsquerdaDepoimentos(){
+    // Telas: 599px-768px
+    if (telaNormal.matches){
+        // Se estiver no primeiro card e então clicar para mover para esquerda, será mostrado o último card do carrossel
+        if (containerSec8.scrollLeft <= 0) {
+            containerSec8.scrollLeft = containerSec8.scrollWidth - containerSec8.clientWidth;
+        } 
+        // Se não for o primeiro card, será mostrado o card anterior
+        else {
+            containerSec8.scrollLeft -= 250; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card.
+        }
+    }
+}
+btnSec8CarrosselEsquerda.addEventListener('click', moverEsquerdaDepoimentos);
