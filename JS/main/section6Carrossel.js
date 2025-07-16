d1d7e1e61d50d198d97263522384e81eea4bb8b5
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-15 23:46:38 -0300

    Adição da responsividade na section 6 (599px-688px)
    - Fiz uma correção para telas 689px-798px.

--- JS/main/section6Carrossel.js
+++ JS/main/section6Carrossel.js
@@ -4,17 +4,20 @@ const containerSec6 = document.querySelector('#section6container');
 
 // Media query
 /*
-Tela: 599px-768px = telaNormal
-Tela: 390px-598px = telaNormalPequena
+Tela: 689px-768px = atributos_telaNormal
+Tela: 390px-598px = atributos_telaNormalPequena
 Tela: ...px-389px = telaMuitoPequena
 */
+
+const atributos_telaNormal         = window.matchMedia('(min-width: 689px) and (max-width: 798px)');
+const atributos_telaNormalPequena   = window.matchMedia('(min-width: 599px) and (max-width: 688px)');
 
 // Mover itens do carrossel para direita
 function moverDireitaAtributos(){
     const scrollMax = containerSec6.scrollWidth - containerSec6.clientWidth;
 
     // Telas: 689px-768px
-    if (telaNormal.matches){
+    if (atributos_telaNormal.matches){
         // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
         if (containerSec6.scrollLeft >= scrollMax) {
             containerSec6.scrollLeft = 0;
@@ -24,13 +27,21 @@ function moverDireitaAtributos(){
             containerSec6.scrollLeft += 270; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card.
         }
     }
+    // Telas: 599px-688px
+    else if (atributos_telaNormalPequena.matches){
+        if (containerSec6.scrollLeft >= scrollMax) {
+            containerSec6.scrollLeft = 0;
+        } else {
+            containerSec6.scrollLeft += 250;
+        }
+    }
 }
 btnSec6CarrosselDireita.addEventListener('click', moverDireitaAtributos);
 
 // Mover itens do carrossel para esquerda
 function moverEsquerdaAtributos(){
     // Telas: 689px-768px
-    if (telaNormal.matches){
+    if (atributos_telaNormalPequena.matches){
         // Se estiver no primeiro card e então clicar para mover para esquerda, será mostrado o último card do carrossel
         if (containerSec6.scrollLeft <= 0) {
             containerSec6.scrollLeft = containerSec6.scrollWidth - containerSec6.clientWidth;
@@ -38,6 +49,14 @@ function moverEsquerdaAtributos(){
         // Se não for o primeiro card, será mostrado o card anterior
         else {
             containerSec6.scrollLeft -= 270; // Esse nº vem do width do card + gap entre os cards, então ele avançará um card.
+        }
+    }
+    // Telas: 599px-688px
+    else if(telaNormalPequena.matches){
+        if (containerSec8.scrollLeft <= 0) {
+            containerSec8.scrollLeft = containerSec8.scrollWidth - containerSec8.clientWidth;
+        } else {
+            containerSec8.scrollLeft -= 250;
         }
     }
 }
