fb93fb7776f0ff63ad875d36e9a356167b019471
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-15 23:58:41 -0300

    Alteração de nomes de algumas variáveis do carrossel Depoimentos
    - Alterei o nome das variáveis que capturam a largura da tela para que não houvesse conflito de variáveis de outros arquivos, mesmo estando em arquivos JS diferentes.

--- JS/main/section8Carrossel.js
+++ JS/main/section8Carrossel.js
@@ -4,20 +4,20 @@ const containerSec8 = document.querySelector('#section8Cards');
 
 // Media query
 /*
-Tela: 599px-768px = telaNormal
-Tela: 390px-598px = telaNormalPequena
-Tela: ...px-389px = telaMuitoPequena
+Tela: 599px-768px = depoimentos_telaNormal
+Tela: 390px-598px = depoimentos_telaNormalPequena
+Tela: ...px-389px = atributos_telaMuitoPequena
 */
-const telaNormal         = window.matchMedia('(min-width: 599px) and (max-width: 768px)');
-const telaNormalPequena  = window.matchMedia('(min-width: 390px) and (max-width: 598px)');
-const telaMuitoPequena   = window.matchMedia('(max-width: 389px)');
+const depoimentos_telaNormal         = window.matchMedia('(min-width: 599px) and (max-width: 768px)');
+const depoimentos_telaNormalPequena  = window.matchMedia('(min-width: 390px) and (max-width: 598px)');
+const atributos_telaMuitoPequena   = window.matchMedia('(max-width: 389px)');
 
 // Mover itens do carrossel para direita
 function moverDireitaDepoimentos(){
     const scrollMax = containerSec8.scrollWidth - containerSec8.clientWidth;
 
     // Telas: 599px-768px
-    if (telaNormal.matches){
+    if (depoimentos_telaNormal.matches){
         // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
         if (containerSec8.scrollLeft >= scrollMax) {
             containerSec8.scrollLeft = 0;
@@ -28,7 +28,7 @@ function moverDireitaDepoimentos(){
         }
     } 
     // Telas: 390px-598px
-    else if (telaNormalPequena.matches){
+    else if (depoimentos_telaNormalPequena.matches){
         // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
         if (containerSec8.scrollLeft >= scrollMax) {
             containerSec8.scrollLeft = 0;
@@ -39,7 +39,7 @@ function moverDireitaDepoimentos(){
         }
     }
     // Telas: ...px-389px
-    else if (telaMuitoPequena.matches){
+    else if (atributos_telaMuitoPequena.matches){
         // Se estiver no último card e então clicar para mover para direita, será mostrado o 1º card do carrossel
         if (containerSec8.scrollLeft >= scrollMax) {
             containerSec8.scrollLeft = 0;
@@ -55,7 +55,7 @@ btnSec8CarrosselDireita.addEventListener('click', moverDireitaDepoimentos);
 // Mover itens do carrossel para esquerda
 function moverEsquerdaDepoimentos(){
     // Telas: 599px-768px
-    if (telaNormal.matches){
+    if (depoimentos_telaNormal.matches){
         // Se estiver no primeiro card e então clicar para mover para esquerda, será mostrado o último card do carrossel
         if (containerSec8.scrollLeft <= 0) {
             containerSec8.scrollLeft = containerSec8.scrollWidth - containerSec8.clientWidth;
@@ -66,7 +66,7 @@ function moverEsquerdaDepoimentos(){
         }
     }
     // Telas: 390px-598px
-    else if(telaNormalPequena.matches){
+    else if (depoimentos_telaNormalPequena.matches){
         // Se estiver no primeiro card e então clicar para mover para esquerda, será mostrado o último card do carrossel
         if (containerSec8.scrollLeft <= 0) {
             containerSec8.scrollLeft = containerSec8.scrollWidth - containerSec8.clientWidth;
@@ -77,7 +77,7 @@ function moverEsquerdaDepoimentos(){
         }
     }
     // Telas: ...px-389px
-    else if(telaMuitoPequena.matches){
+    else if (telaMuitoPequena.matches){
         // Se estiver no primeiro card e então clicar para mover para esquerda, será mostrado o último card do carrossel
         if (containerSec8.scrollLeft <= 0) {
             containerSec8.scrollLeft = containerSec8.scrollWidth - containerSec8.clientWidth;
