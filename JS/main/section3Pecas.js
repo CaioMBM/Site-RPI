1393900b0708a0838eea0bd400c5fa047ee6c7dd
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-17 10:54:15 -0300

    Adição do JS na peça I da section 3
    - Adição da imagem da peça I (c/fundo vermelho);
    - Renomeei a peça I (c/fundo transparente),
    
    Obs: Falta fazer o mesmo para a peça R.

--- JS/main/section3Pecas.js
+++ JS/main/section3Pecas.js
@@ -1,20 +1,42 @@
-const pecaQC_Perfor  = document.getElementById('pecaQC_Perfor');
 const conteinarPecas = document.getElementById('conteinarPecas');
+const fundoPadrao    = 'Imagens/Quebra-cabeça/fundoPecasPadrao.png';
 
+// Performance
 // Caminhos relativos ao HTML
-const imagem1 = 'Imagens/Quebra-cabeça/peçaPerfor-Contorno.png';
-const imagem2 = 'Imagens/Quebra-cabeça/peçaPerfor-Clicada.png';
-const fundoPadrao = 'Imagens/Quebra-cabeça/fundoPecasPadrao.png';
+const pecaQC_Perfor  = document.getElementById('pecaQC_Perfor');
+const imagem1     = 'Imagens/Quebra-cabeça/peçaPerfor-Contorno.png';
+const imagem2     = 'Imagens/Quebra-cabeça/peçaPerfor-Clicada.png';
 const fundoPerfor = 'Imagens/Quebra-cabeça/QC-Performance_resized.png';
 
 pecaQC_Perfor.addEventListener('click', () => {
     const imgAtual = pecaQC_Perfor.getAttribute('src');
-
+    
     if (imgAtual === imagem1) {
         pecaQC_Perfor.setAttribute('src', imagem2);
         conteinarPecas.style.backgroundImage = `url('${fundoPerfor}')`;
     } else {
         pecaQC_Perfor.setAttribute('src', imagem1);
+        conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
+    }
+});
+
+
+// Interferência
+// Caminhos relativos ao HTML
+const pecaQC_Interf = document.getElementById('pecaQC_Interf');
+const imagem3     = 'Imagens/Quebra-cabeça/peçaInterf-Contorno.png';
+const imagem4     = 'Imagens/Quebra-cabeça/peçaInterf-Clicada.png';
+const fundoInterf = 'Imagens/Quebra-cabeça/QC-Interferencia_resized.png';
+
+pecaQC_Interf.addEventListener('click', () => {
+    console.log('oi')
+    const imgAgora = pecaQC_Interf.getAttribute('src');
+    
+    if (imgAgora === imagem3) {
+        pecaQC_Interf.setAttribute('src', imagem4);
+        conteinarPecas.style.backgroundImage = `url('${fundoInterf}')`;
+    } else {
+        pecaQC_Interf.setAttribute('src', imagem3);
         conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
     }
 });
