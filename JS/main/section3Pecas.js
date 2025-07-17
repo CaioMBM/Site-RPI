5b545026383e638cb750b2a33243c78f10be6f0f
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-17 11:49:44 -0300

    Adição da alternância de imagem da peça quando for clicada
    - Alterei o nome de algumas variáveis para seguir a um padrão.
    - Agora o fundo da imagem altera independentemente de qual peça for clicada.

--- JS/main/section3Pecas.js
+++ JS/main/section3Pecas.js
@@ -1,21 +1,44 @@
 const conteinarPecas = document.getElementById('conteinarPecas');
 const fundoPadrao    = 'Imagens/Quebra-cabeça/fundoPecasPadrao.png';
 
+// Resultado
+// Caminhos relativos ao HTML
+const pecaQC_Result = document.getElementById('pecaQC_Result');
+const imgResult_Contorno = 'Imagens/Quebra-cabeça/peçaResult-Contorno.png';
+const imgResult_Clicada  = 'Imagens/Quebra-cabeça/peçaResult-Clicada.png';
+const fundoResult = 'Imagens/Quebra-cabeça/QC-Resultado_resized.png';
+
+pecaQC_Result.addEventListener('click', () => {
+    const imgPresente = pecaQC_Result.getAttribute('src');
+    
+    if (imgPresente === imgResult_Contorno) {
+        pecaQC_Result.setAttribute('src', imgResult_Clicada);
+        pecaQC_Perfor.setAttribute('src', imgPerfor_Contorno);
+        pecaQC_Interf.setAttribute('src', imgInterf_Contorno);
+        conteinarPecas.style.backgroundImage = `url('${fundoResult}')`;
+    } else {
+        pecaQC_Result.setAttribute('src', imgResult_Contorno);
+        conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
+    }
+});
+
 // Performance
 // Caminhos relativos ao HTML
 const pecaQC_Perfor  = document.getElementById('pecaQC_Perfor');
-const imagem1     = 'Imagens/Quebra-cabeça/peçaPerfor-Contorno.png';
-const imagem2     = 'Imagens/Quebra-cabeça/peçaPerfor-Clicada.png';
+const imgPerfor_Contorno = 'Imagens/Quebra-cabeça/peçaPerfor-Contorno.png';
+const imgPerfor_Clicada  = 'Imagens/Quebra-cabeça/peçaPerfor-Clicada.png';
 const fundoPerfor = 'Imagens/Quebra-cabeça/QC-Performance_resized.png';
 
 pecaQC_Perfor.addEventListener('click', () => {
     const imgAtual = pecaQC_Perfor.getAttribute('src');
     
-    if (imgAtual === imagem1) {
-        pecaQC_Perfor.setAttribute('src', imagem2);
+    if (imgAtual === imgPerfor_Contorno) {
+        pecaQC_Result.setAttribute('src', imgResult_Contorno);
+        pecaQC_Perfor.setAttribute('src', imgPerfor_Clicada);
+        pecaQC_Interf.setAttribute('src', imgInterf_Contorno);
         conteinarPecas.style.backgroundImage = `url('${fundoPerfor}')`;
     } else {
-        pecaQC_Perfor.setAttribute('src', imagem1);
+        pecaQC_Perfor.setAttribute('src', imgPerfor_Contorno);
         conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
     }
 });
@@ -24,37 +47,20 @@ pecaQC_Perfor.addEventListener('click', () => {
 // Interferência
 // Caminhos relativos ao HTML
 const pecaQC_Interf = document.getElementById('pecaQC_Interf');
-const imagem3     = 'Imagens/Quebra-cabeça/peçaInterf-Contorno.png';
-const imagem4     = 'Imagens/Quebra-cabeça/peçaInterf-Clicada.png';
+const imgInterf_Contorno = 'Imagens/Quebra-cabeça/peçaInterf-Contorno.png';
+const imgInterf_Clicada  = 'Imagens/Quebra-cabeça/peçaInterf-Clicada.png';
 const fundoInterf = 'Imagens/Quebra-cabeça/QC-Interferencia_resized.png';
 
 pecaQC_Interf.addEventListener('click', () => {
     const imgAgora = pecaQC_Interf.getAttribute('src');
     
-    if (imgAgora === imagem3) {
-        pecaQC_Interf.setAttribute('src', imagem4);
+    if (imgAgora === imgInterf_Contorno) {
+        pecaQC_Result.setAttribute('src', imgResult_Contorno);
+        pecaQC_Perfor.setAttribute('src', imgPerfor_Contorno);
+        pecaQC_Interf.setAttribute('src', imgInterf_Clicada);
         conteinarPecas.style.backgroundImage = `url('${fundoInterf}')`;
     } else {
-        pecaQC_Interf.setAttribute('src', imagem3);
-        conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
-    }
-});
-
-// Interferência
-// Caminhos relativos ao HTML
-const pecaQC_Result = document.getElementById('pecaQC_Result');
-const imagem5     = 'Imagens/Quebra-cabeça/peçaResult-Contorno.png';
-const imagem6     = 'Imagens/Quebra-cabeça/peçaResult-Clicada.png';
-const fundoResult = 'Imagens/Quebra-cabeça/QC-Resultado_resized.png';
-
-pecaQC_Result.addEventListener('click', () => {
-    const imgPresente = pecaQC_Result.getAttribute('src');
-    
-    if (imgPresente === imagem5) {
-        pecaQC_Result.setAttribute('src', imagem6);
-        conteinarPecas.style.backgroundImage = `url('${fundoResult}')`;
-    } else {
-        pecaQC_Result.setAttribute('src', imagem5);
+        pecaQC_Interf.setAttribute('src', imgInterf_Contorno);
         conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
     }
 });
