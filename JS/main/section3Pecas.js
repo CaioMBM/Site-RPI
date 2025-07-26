ae60d129210969e84225f8b0e58569aa3013f508
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-25 23:30:54 -0300

    Adição do conteúdo da peça R na section 3
    - Foi estilizado com CSS e também criado com JS a lógica para exibir e ocultar o conteúdo da peça e o seu modal.

--- JS/main/section3Pecas.js
+++ JS/main/section3Pecas.js
@@ -1,6 +1,6 @@
+const escurecer      = document.getElementById('escurecer');
 const conteinarPecas = document.getElementById('conteinarPecas');
 const fundoPadrao    = 'Imagens/Quebra-cabeça/fundoPecasPadrao.png';
-
 
 // Resultado
 // Caminhos relativos ao HTML
@@ -8,6 +8,10 @@ const pecaQC_Result = document.getElementById('pecaQC_Result');
 const imgResult_Contorno = 'Imagens/Quebra-cabeça/peçaResult-Contorno.png';
 const imgResult_Clicada  = 'Imagens/Quebra-cabeça/peçaResult-Clicada.png';
 const fundoResult = 'Imagens/Quebra-cabeça/QC-Resultado_resized.png';
+const modalResultado     = document.querySelectorAll('.modal')[0];
+const infoResultado      = document.querySelectorAll('.pecaInformacao')[0];
+const btn_resultado      = document.querySelector('#btn-resultado');
+const sairModalResultado = document.querySelectorAll('.sairModal')[0];
 
 pecaQC_Result.addEventListener('click', () => {
     const imgPresente = pecaQC_Result.getAttribute('src');
@@ -17,11 +21,24 @@ pecaQC_Result.addEventListener('click', () => {
         pecaQC_Perfor.setAttribute('src', imgPerfor_Contorno);
         pecaQC_Interf.setAttribute('src', imgInterf_Contorno);
         conteinarPecas.style.backgroundImage = `url('${fundoResult}')`;
+        infoResultado.style.display = 'block';
     } else {
         pecaQC_Result.setAttribute('src', imgResult_Contorno);
         conteinarPecas.style.backgroundImage = `url('${fundoPadrao}')`;
+        infoResultado.style.display = 'none';
     }
 });
+
+// Abrir modal
+btn_resultado.addEventListener('click', () => {
+    modalResultado.style.display = 'block';
+    escurecer.style.display      = 'block';
+})
+// Fechar modal
+sairModalResultado.addEventListener('click', () => {
+    modalResultado.style.display = 'none';
+    escurecer.style.display      = 'none';
+})
 
 
 // Performance
