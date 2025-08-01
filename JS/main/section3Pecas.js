f5d4aec633e44627a80525c78b868080a870cac9
Caio Montenegro <caiombmonteiro@gmail.com>
2025-07-31 22:38:42 -0300

    Ajuste da responsividade da caixa das peças na section 3 (459px-598px)

--- JS/main/section3Pecas.js
+++ JS/main/section3Pecas.js
@@ -3,6 +3,10 @@ const fundoPadrao    = 'Imagens/Quebra-cabeça/fundoPecasPadrao.jpg';
 const infoPadrao     = document.querySelectorAll('.pecaInformacao')[0];
 const escurecer      = document.getElementById('escurecer');
 
+// Media query
+const tela_459_598 = window.matchMedia('(min-width: 459px) and (max-width: 598px)');
+
+
 // Resultado
 // Caminhos relativos ao HTML
 const pecaQC_Result      = document.getElementById('pecaQC_Result');
@@ -12,35 +16,34 @@ const btn_resultado      = document.querySelector('#btn-resultado');
 const sairModalResultado = document.querySelectorAll('.sairModal')[0];
 
 pecaQC_Result.addEventListener('click', () => {
-    // Ao clicar na peça, irá mostrar seu conetúdo e esconder os demais caso tenha sido clicado em outra peça antes dessa
-    if(pecaQC_Result.offsetHeight === 139){
-        // Sombra da caixa
-        conteinarPecas.style.boxShadow = '0 0 20px 5px #030D0F';
+    // Tela entre 459px - 598px
+    if(tela_459_598.matches){
+        if(pecaQC_Result.offsetHeight === 119){
+                // Sombra da caixa
+                conteinarPecas.style.boxShadow = '0 0 20px 5px #030D0F';
 
-        // Peça Resultado
-        pecaQC_Result.style.height    = '160px';
-        pecaQC_Result.style.transform = 'translateX(-10px)';
-        // Peça Performance
-        pecaQC_Perfor.style.height    = '113px';
-        pecaQC_Perfor.style.transform = 'translateY(0)';
-        // Peça Interferência
-        pecaQC_Interf.style.height    = '139px';
-        pecaQC_Interf.style.transform = 'translateX(-32px)';
+                // Peça Resultado
+                pecaQC_Result.style.height    = '139px';
+                pecaQC_Result.style.transform = 'translateX(-5px)';
+                // Peça Performance
+                pecaQC_Perfor.style.height    = '96px';
+                pecaQC_Perfor.style.transform = 'translateY(0)';
+                // Peça Interferência
+                pecaQC_Interf.style.height    = '119px';
+                pecaQC_Interf.style.transform = 'translateX(-32px)';
 
-        // Conteúdo da peça clicada
-        infoPadrao.style.display        = 'none';
-        infoResultado.style.display     = 'block';
-        infoPerformance.style.display   = 'none';
-        infoInterferencia.style.display = 'none';
-    } 
-        // Serve para quando clicar novamente na peça, voltar para o conteúdo default
-        else{
+                // Conteúdo da peça clicada
+                infoPadrao.style.display        = 'none';
+                infoResultado.style.display     = 'block';
+                infoPerformance.style.display   = 'none';
+                infoInterferencia.style.display = 'none';
+        } else{
             // Sombra da caixa
             conteinarPecas.style.boxShadow = '0 0 20px 0 #8aa4a7';
 
             // Peça Resultado
-            pecaQC_Result.style.height    = '139px';
-            pecaQC_Result.style.transform = 'translateX(32px)';
+            pecaQC_Result.style.height    = '119px';
+            pecaQC_Result.style.transform = 'translateX(28px)';
 
             // Conteúdo da peça clicada
             infoPadrao.style.display        = 'block';
@@ -48,6 +51,46 @@ pecaQC_Result.addEventListener('click', () => {
             infoPerformance.style.display   = 'none';
             infoInterferencia.style.display = 'none';
         }
+    } 
+    // Telas de no mínimo 599px
+    else{
+        // Ao clicar na peça, irá mostrar seu conetúdo e esconder os demais caso tenha sido clicado em outra peça antes dessa
+        if(pecaQC_Result.offsetHeight === 139){
+            // Sombra da caixa
+            conteinarPecas.style.boxShadow = '0 0 20px 5px #030D0F';
+
+            // Peça Resultado
+            pecaQC_Result.style.height    = '160px';
+            pecaQC_Result.style.transform = 'translateX(-10px)';
+            // Peça Performance
+            pecaQC_Perfor.style.height    = '113px';
+            pecaQC_Perfor.style.transform = 'translateY(0)';
+            // Peça Interferência
+            pecaQC_Interf.style.height    = '139px';
+            pecaQC_Interf.style.transform = 'translateX(-32px)';
+
+            // Conteúdo da peça clicada
+            infoPadrao.style.display        = 'none';
+            infoResultado.style.display     = 'block';
+            infoPerformance.style.display   = 'none';
+            infoInterferencia.style.display = 'none';
+        } 
+            // Serve para quando clicar novamente na peça, voltar para o conteúdo default
+            else{
+                // Sombra da caixa
+                conteinarPecas.style.boxShadow = '0 0 20px 0 #8aa4a7';
+
+                // Peça Resultado
+                pecaQC_Result.style.height    = '139px';
+                pecaQC_Result.style.transform = 'translateX(32px)';
+
+                // Conteúdo da peça clicada
+                infoPadrao.style.display        = 'block';
+                infoResultado.style.display     = 'none';
+                infoPerformance.style.display   = 'none';
+                infoInterferencia.style.display = 'none';
+            }
+    }
 });
 
 // Abrir modal
@@ -71,39 +114,38 @@ const btn_Performance      = document.querySelector('#btn-performance');
 const sairModalPerformance = document.querySelectorAll('.sairModal')[1];
 
 pecaQC_Perfor.addEventListener('click', () => {
-    // Ao clicar na peça, irá mostrar seu conetúdo e esconder os demais caso tenha sido clicado em outra peça antes dessa
-    if(pecaQC_Perfor.offsetHeight === 113){
-        // Sombra da caixa
-        conteinarPecas.style.boxShadow = '0 0 20px 5px #11BCE8';
+    // Tela entre 459px - 598px
+    if(tela_459_598.matches){
+        if(pecaQC_Perfor.offsetHeight === 96){
+                // Sombra da caixa
+                conteinarPecas.style.boxShadow = '0 0 20px 5px #11BCE8';
 
-        // Peça Resultado
-        pecaQC_Result.style.height    = '139px';
-        pecaQC_Result.style.transform = 'translateX(-10px)';
-        
-        // Peça clicada
-        pecaQC_Perfor.style.height   = '140px';
-        pecaQC_Perfor.style.transform = 'translateY(-10px)';
+                // Peça Resultado
+                pecaQC_Result.style.height    = '119px';
+                pecaQC_Result.style.transform = 'translateX(-10px)';
+                // Peça Performance
+                pecaQC_Perfor.style.height    = '106px';
+                pecaQC_Perfor.style.transform = 'translateY(-15px)';
+                // Peça Interferência
+                pecaQC_Interf.style.height    = '119px';
+                pecaQC_Interf.style.transform = 'translateX(5px)';
 
-        // Peça Resultado
-        pecaQC_Interf.style.height    = '139px';
-        pecaQC_Interf.style.transform = 'translateX(10px)';
-        
-        // Conteúdo da peça clicada
-        infoPadrao.style.display        = 'none';
-        infoResultado.style.display     = 'none';
-        infoPerformance.style.display   = 'block';
-        infoInterferencia.style.display = 'none';
-    }
-        // Serve para quando clicar novamente na peça, voltar para o conteúdo default
-        else{
+                // Conteúdo da peça clicada
+                infoPadrao.style.display        = 'none';
+                infoResultado.style.display     = 'none';
+                infoPerformance.style.display   = 'block';
+                infoInterferencia.style.display = 'none';
+        } else{
             // Sombra da caixa
-            conteinarPecas.style.boxShadow = '0 0 20px 0 #8aa4a7';
+            conteinarPecas.style.boxShadow = '0 0 20px 5px #8aa4a7';
 
-            // Peça clicada
-            pecaQC_Perfor.style.height   = '113px';
-            pecaQC_Result.style.transform = 'translateX(32px)';
-            pecaQC_Perfor.style.transform = 'translateY(0px)';
-            pecaQC_Interf.style.transform = 'translateX(-32px)';
+            // Peça Resultado
+            pecaQC_Result.style.transform = 'translateX(28px)';
+            // Peça Performance
+            pecaQC_Perfor.style.height    = '96px';
+            pecaQC_Perfor.style.transform = 'translateY(0)';
+            // Peça Interferência
+            pecaQC_Interf.style.transform = 'translateX(-28px)';
 
             // Conteúdo da peça clicada
             infoPadrao.style.display        = 'block';
@@ -111,6 +153,51 @@ pecaQC_Perfor.addEventListener('click', () => {
             infoPerformance.style.display   = 'none';
             infoInterferencia.style.display = 'none';
         }
+    } 
+    // Telas de no mínimo 599px
+    else{
+        // Ao clicar na peça, irá mostrar seu conetúdo e esconder os demais caso tenha sido clicado em outra peça antes dessa
+        if(pecaQC_Perfor.offsetHeight === 113){
+            // Sombra da caixa
+            conteinarPecas.style.boxShadow = '0 0 20px 5px #11BCE8';
+    
+            // Peça Resultado
+            pecaQC_Result.style.height    = '139px';
+            pecaQC_Result.style.transform = 'translateX(-10px)';
+            
+            // Peça clicada
+            pecaQC_Perfor.style.height   = '140px';
+            pecaQC_Perfor.style.transform = 'translateY(-10px)';
+    
+            // Peça Resultado
+            pecaQC_Interf.style.height    = '139px';
+            pecaQC_Interf.style.transform = 'translateX(10px)';
+            
+            // Conteúdo da peça clicada
+            infoPadrao.style.display        = 'none';
+            infoResultado.style.display     = 'none';
+            infoPerformance.style.display   = 'block';
+            infoInterferencia.style.display = 'none';
+        }
+            // Serve para quando clicar novamente na peça, voltar para o conteúdo default
+            else{
+                // Sombra da caixa
+                conteinarPecas.style.boxShadow = '0 0 20px 0 #8aa4a7';
+    
+                // Peça clicada
+                pecaQC_Perfor.style.height   = '113px';
+                pecaQC_Result.style.transform = 'translateX(32px)';
+                pecaQC_Perfor.style.transform = 'translateY(0px)';
+                pecaQC_Interf.style.transform = 'translateX(-32px)';
+    
+                // Conteúdo da peça clicada
+                infoPadrao.style.display        = 'block';
+                infoResultado.style.display     = 'none';
+                infoPerformance.style.display   = 'none';
+                infoInterferencia.style.display = 'none';
+            }
+    }
+
 });
 
 // Abrir modal
@@ -134,36 +221,35 @@ const btn_Interferencia      = document.querySelector('#btn-interferencia');
 const sairModalInterferencia = document.querySelectorAll('.sairModal')[2];
 
 pecaQC_Interf.addEventListener('click', () => {
-    // Ao clicar na peça, irá mostrar seu conetúdo e esconder os demais caso tenha sido clicado em outra peça antes dessa
-    if(pecaQC_Interf.offsetHeight === 139){
-        // Sombra da caixa
-        conteinarPecas.style.boxShadow = '0 0 20px 5px #C32224';
-
-        // Peça Resultado
-        pecaQC_Result.style.height    = '139px';
-        pecaQC_Result.style.transform = 'translateX(32px)';
-
-        // Peça Performance
-        pecaQC_Perfor.style.height    = '113px';
-        pecaQC_Perfor.style.transform = 'translateY(0)';
-
-        // Peça clicada
-        pecaQC_Interf.style.height = '160px';
-        pecaQC_Interf.style.transform = 'translateX(10px)';
-
-        // Conteúdo da peça clicada
-        infoPadrao.style.display        = 'none';
-        infoResultado.style.display     = 'none';
-        infoPerformance.style.display   = 'none';
-        infoInterferencia.style.display = 'block';
-    } 
-        // Serve para quando clicar novamente na peça, voltar para o conteúdo default
-        else{
+    // Tela entre 459px - 598px
+    if(tela_459_598.matches){
+        if(pecaQC_Interf.offsetHeight === 119){
             // Sombra da caixa
-            conteinarPecas.style.boxShadow = '0 0 20px 0 #8aa4a7';
+            conteinarPecas.style.boxShadow = '0 0 20px 5px #C32224';
+
+            // Peça Resultado
+            pecaQC_Result.style.height    = '119px';
+            pecaQC_Result.style.transform = 'translateX(32px)';
+
+            // Peça Performance
+            pecaQC_Perfor.style.height    = '96px';
+            pecaQC_Perfor.style.transform = 'translateY(0)';
 
             // Peça clicada
-            pecaQC_Interf.style.height    = '139px';
+            pecaQC_Interf.style.height = '139px';
+            pecaQC_Interf.style.transform = 'translateX(5px)';
+
+            // Conteúdo da peça clicada
+            infoPadrao.style.display        = 'none';
+            infoResultado.style.display     = 'none';
+            infoPerformance.style.display   = 'none';
+            infoInterferencia.style.display = 'block';
+        } else{
+            // Sombra da caixa
+            conteinarPecas.style.boxShadow = '0 0 20px 5px #8aa4a7';
+
+            // Peça Resultado
+            pecaQC_Interf.style.height    = '119px';
             pecaQC_Interf.style.transform = 'translateX(-32px)';
 
             // Conteúdo da peça clicada
@@ -172,6 +258,48 @@ pecaQC_Interf.addEventListener('click', () => {
             infoPerformance.style.display   = 'none';
             infoInterferencia.style.display = 'none';
         }
+    } 
+    // Telas de no mínimo 599px
+    else{
+        // Ao clicar na peça, irá mostrar seu conetúdo e esconder os demais caso tenha sido clicado em outra peça antes dessa
+        if(pecaQC_Interf.offsetHeight === 139){
+            // Sombra da caixa
+            conteinarPecas.style.boxShadow = '0 0 20px 5px #C32224';
+    
+            // Peça Resultado
+            pecaQC_Result.style.height    = '139px';
+            pecaQC_Result.style.transform = 'translateX(32px)';
+    
+            // Peça Performance
+            pecaQC_Perfor.style.height    = '113px';
+            pecaQC_Perfor.style.transform = 'translateY(0)';
+    
+            // Peça clicada
+            pecaQC_Interf.style.height = '160px';
+            pecaQC_Interf.style.transform = 'translateX(10px)';
+    
+            // Conteúdo da peça clicada
+            infoPadrao.style.display        = 'none';
+            infoResultado.style.display     = 'none';
+            infoPerformance.style.display   = 'none';
+            infoInterferencia.style.display = 'block';
+        } 
+            // Serve para quando clicar novamente na peça, voltar para o conteúdo default
+            else{
+                // Sombra da caixa
+                conteinarPecas.style.boxShadow = '0 0 20px 0 #8aa4a7';
+    
+                // Peça clicada
+                pecaQC_Interf.style.height    = '139px';
+                pecaQC_Interf.style.transform = 'translateX(-32px)';
+    
+                // Conteúdo da peça clicada
+                infoPadrao.style.display        = 'block';
+                infoResultado.style.display     = 'none';
+                infoPerformance.style.display   = 'none';
+                infoInterferencia.style.display = 'none';
+            }
+    }
 });
 
 // Abrir modal
