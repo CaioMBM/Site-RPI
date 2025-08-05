355711a677b91b8d1265ce5205a4be8874823024
Caio Montenegro <caiombmonteiro@gmail.com>
2025-08-05 15:16:06 -0300

    Correção da lógica pra navbar fechar
    - Quando clicava num navlink funcionava no mobile, mas a mesma lógica estava sendo aplicada para desktop e isso estava dando erro.

--- JS/headerResponsivo.js
+++ JS/headerResponsivo.js
@@ -1,6 +1,21 @@
 const navbar          = document.querySelector('nav')
+const navlinks        = document.querySelectorAll('.navlink a')
 const icon_abrirMenu  = document.querySelector('.icon-abrirMenu');
 const icon_fecharMenu = document.querySelector('.icon-fecharMenu');
+const diversasTelas   = window.matchMedia('(max-width: 980px)');
+
+// Media query: ...px - 980px
+if (diversasTelas.matches){
+    navlinks.forEach(function(navlink){
+        navlink.addEventListener('click', () => {
+            setTimeout(() => {
+                icon_abrirMenu.style.display  = 'block';
+                icon_fecharMenu.style.display = 'none';
+                navbar.style.display          = 'none';
+            }, 300);
+        })
+    })
+};
 
 // Abrir menu hamburguer
 icon_abrirMenu.addEventListener("click", ()=> {
@@ -9,20 +24,11 @@ icon_abrirMenu.addEventListener("click", ()=> {
     navbar.style.display          = 'block';
 
     navbar.classList.remove('navbarOcultar');
-})
+});
 
 // Fechar menu hamburguer
 icon_fecharMenu.addEventListener("click", ()=> {
     icon_abrirMenu.style.display  = 'block';
     icon_fecharMenu.style.display = 'none';
     navbar.style.display          = 'none';
-})
-
-function fecharMenu(){
-    setTimeout(() => {
-        icon_abrirMenu.style.display  = 'block';
-        icon_fecharMenu.style.display = 'none';
-        navbar.style.display          = 'none';
-    }, 300);
-    
-}
+});
