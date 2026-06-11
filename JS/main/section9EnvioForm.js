fdc732c8c4675ec2c0b667e938269b4cd53fe29e
Caio Montenegro <caiombmonteiro@gmail.com>
2026-06-11 17:05:15 -0300

    feat(form): Personalização de como as informações do e-mail chegam a caixa de e-mails
    - Agora aparece quem enviou e o assunto do e-mail logo de cara, antes mesmo de abrir o e-mail. E quando abrir o e-mail, as informações continuarão separadas e organizadas.

--- JS/main/section9EnvioForm.js
+++ JS/main/section9EnvioForm.js
@@ -1,4 +1,4 @@
-// Variáveis Global
+// Variáveis Globais
 const formContainer = document.querySelector('form');
 const formBtnEnviar = document.querySelector('form button');
 
@@ -10,8 +10,21 @@ formContainer.addEventListener('submit', async (e) => {
     const formData = new FormData(formContainer);
     formData.append("access_key", "ef9920a6-0592-45f0-a762-f63379f617a3"); // Web3Forms - Chave de acesso
 
-    const originalText = formBtnEnviar.textContent;
 
+    // ======== Personaliza o e-mail que chega após enviar pelo form do site ========
+    
+    // Pega o que o usuário digitou no campo "Nome"
+    const usuarioNomeu = formData.get("Nome"); 
+    // Pega o que o usuário digitou no campo "Assunto"
+    const usuarioAssunto = formData.get("Assunto") || "Nova mensagem do site"; 
+    // Quando receber um e-mail vindo do form do site, vai aparecer logo de cara quem enviou e o assunto da mensagem.
+    formData.set("subject", `De: ${usuarioNomeu} | ${usuarioAssunto}`);
+    
+    // ======== FIM ========
+
+
+    // Altera o texto do botão após clicar em "Enviar", desabilita o botão e só volta ao normal após fechar o alert que aparece
+    const originalText = formBtnEnviar.textContent;
     formBtnEnviar.textContent = "Enviando e-mail...";
     formBtnEnviar.disabled = true;
 
